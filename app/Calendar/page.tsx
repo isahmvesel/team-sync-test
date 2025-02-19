8cc057139c739b79b01cfb3c3daba0e184a73cbc
stefanbadiu <sbadiu@purdue.edu>
2025-02-19 09:52:45 -0500

    firebase config, login page that sends request, plus placeholder pages for register/calendar with redirects between

---
+++ app/Calendar/page.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+export default function Register() {
+    return (
+        <div className="bg-[rgb(230,230,230)] grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center">
+                <h1 className="text-black text-center text-5xl underline text-bold">Calendar (Placeholder page)</h1>
+
+                <Link
+                className="rounded-full border border-solid border-black/[.08] dark:border-black/[.70] text-black transition-colors flex items-center justify-center hover:bg-[#f2f2f2] hover:text-[#ffffff] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-8 sm:h-10 px-3 sm:px-4 sm:min-w-40"
+                href="/" // Main page
+                //target="_blank"
+                rel="noopener noreferrer"
+                >
+                    Back
+                </Link>
+            </main>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,48 @@
+"use client"
 import Image from "next/image";
+import Link from 'next/link';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from 'react';
+import { auth } from "../app/Firebase/config";
+
 
 export default function Home() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  //const auth = getAuth();
+
+  const changeUsernameInput = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const changePasswordInput = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleLogin = async () => {
+    try {
+      console.log('trying login | USER:' + username + "@dummydomain.com | PASSWORD:" + password + "\n");
+      const response = await fetch( "/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+      if (!response.ok) throw new Error(data.error);
+
+      await signInWithEmailAndPassword(auth, username + "@dummydomain.com", password);
+      console.log('User signed in: ' + { username, password });
+      //TODO: Bind calendar page navigation to successful login, 
+    } catch (err: any) {
+      setError(err.message);
+      console.log('login failed\n');
+      //TODO: Display error message on fail
+    }
+  }
+
   return (
     <div className="bg-[rgb(230,230,230)] grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
@@ -14,6 +56,8 @@ export default function Home() {
             style={{ backgroundColor: "rgb(180, 180, 180)" , borderColor: "rgb(100, 100, 100)"}}
             className="border rounded px-2 py-1 text-black placeholder-[rgb(70,70,70)]"
             placeholder="Enter username"
+            value={username}
+            onChange={changeUsernameInput}
           />
           </li>
 
@@ -24,6 +68,8 @@ export default function Home() {
             style={{ backgroundColor: "rgb(180, 180, 180)" , borderColor: "rgb(100, 100, 100)"}}
             className="border rounded px-2 py-1 text-black placeholder-[rgb(70,70,70)]"
             placeholder="Enter password"
+            value={password}
+            onChange={changePasswordInput}
           />
           </li>
         </ul>
@@ -31,9 +77,10 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-black/[.70] text-black transition-colors flex items-center justify-center hover:bg-[#f2f2f2] hover:text-[#ffffff] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-28"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            href="/Calendar"
+            //target="_blank"
             rel="noopener noreferrer"
+            onClick={handleLogin}
           >
             <Image
               className="dark:invert mr-2"
@@ -44,66 +91,17 @@ export default function Home() {
             />
             Log in
           </a>
-          <a
+          <Link
             className="rounded-full border border-solid border-black/[.08] dark:border-black/[.70] text-black transition-colors flex items-center justify-center hover:bg-[#f2f2f2] hover:text-[#ffffff] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            href="/Register"
+            //target="_blank"
             rel="noopener noreferrer"
           >
             Sign up
-          </a>
+          </Link>
         </div>
       </main>
-      <footer className="text-black row-start-3 flex gap-6 flex-wrap items-center justify-between px-10">
-          <ul>
-            <li className="">Stefan Badiu</li>
-          </ul>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      
     </div>
   );
 }
