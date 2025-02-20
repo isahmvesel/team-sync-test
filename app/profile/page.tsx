4670dc89777be9e1ff13d3c3393a89353a533865
637marcoz <zhan4543@purdu.edu>
2025-02-20 09:24:28 -0500

    firebase setup

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,34 @@
-export default function Home() {
+"use client"
+
+import { useState } from 'react';
+import { db } from '../../utils/firebaseConfig';
+import { collection, addDoc } from "firebase/firestore"; 
+
+export default function Profile() {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const docRef = await addDoc(collection(db, "items"), {
+                name: value
+            });
+            console.log("Document written with ID: ", docRef.id);
+            setValue('');
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    };
+
     return (
-        <div>Profile Page</div>    
-    )
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                placeholder="Add a new item"
+            />
+            <button type="submit">Add Item</button>
+        </form>
+    );
 }
