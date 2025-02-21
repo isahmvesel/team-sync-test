0fbbc580b81a45541e23933712e8ebf2e59f335d
637marcoz <zhan4543@purdu.edu>
2025-02-21 15:47:19 -0500

    set/view helpers and demo page

--- utils/firebaseHelper.js
+++ utils/firebaseHelper.js
@@ -1,5 +1,5 @@
 import { db } from "./firebaseConfig.js";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 
 async function setDocument(collectionName, docId, data) {
     try {
@@ -10,4 +10,20 @@ async function setDocument(collectionName, docId, data) {
     }
 }
 
-export { setDocument };
+async function viewDocument(collectionName, docId) {
+    try {
+        const documentRef = doc(db, collectionName, docId);
+        const documentSnapshot = await getDoc(documentRef);
+        if (documentSnapshot.exists()) {
+            return documentSnapshot.data();
+        } else {
+            console.log("No such document!");
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching document: ", error);
+        return null;
+    }
+}
+
+export { setDocument, viewDocument };
