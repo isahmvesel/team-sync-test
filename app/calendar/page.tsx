4f0b9cf08b6710100b6e7cc24fe6454509d7c25f
dylmill8 <mill3170@purdue.edu>
2025-03-01 15:55:16 -0500

    Calendar page next/previous months, view option selection (day, week, & month), and event creation page button + routing

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,13 +1,39 @@
 "use client";
 
+import "./calendar.css";
+
 import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import dayGridPlugin from '@fullcalendar/daygrid'
+import { useRouter } from "next/navigation";
 
 export default function Calendar() {
+  const router = useRouter();
+
   return (
-    <FullCalendar
-      plugins={[ dayGridPlugin ]}
-      initialView="dayGridMonth"
-    />
+    <>
+      <FullCalendar
+        themeSystem='standard'
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        customButtons={{
+          createEvent: {
+            text: 'Create Event',
+            click: () => {
+              router.push('/event/create');
+            },
+            },
+          }}
+          headerToolbar={{
+            left: 'dayGridDay,dayGridWeek,dayGridMonth',
+            center: 'title',
+            right: 'createEvent today prevYear,prev,next,nextYear'
+          }}
+          />
+          <style jsx global>{`
+          .fc .fc-toolbar-title {
+            font-weight: bold;
+          }
+          `}</style>
+    </>
   )
 }
