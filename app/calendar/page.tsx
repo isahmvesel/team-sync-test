bc0857941e237579850e7635fc7f94ec632f3cf6
dylmill8 <mill3170@purdue.edu>
2025-03-01 18:29:28 -0500

    User Event Parsing, Calendar Event Display Testing, & Time Grid For Day/Week Viewing Options

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -4,36 +4,53 @@ import "./calendar.css";
 
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
 import { useRouter } from "next/navigation";
 import { firebaseApp } from "@/utils/firebaseConfig";
 import { db } from '@/utils/firebaseConfig';
-//import { collection, addDoc, Timestamp } from "firebase/firestore";
-//import { getAuth, onAuthStateChanged } from "firebase/auth";
-
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { time } from "console";
 
 export default function Calendar() {
   const router = useRouter();
-  //const auth = getAuth(firebaseApp);
-  //const user = auth.currentUser;
-  //const uid = user.uid;
+  const auth = getAuth(firebaseApp);
 
-  /*
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
+  onAuthStateChanged(auth, async (user) => {
+    //! TODO: fix if statement
+    if (1) {//user) {
+
       // User is signed in, get the UID
-      const uid = user.uid;
-    } else {
-      // User is not signed in, display an error message
-      console.error("User is not signed in.");
-    }
-  });
-  */
+      const user = auth.currentUser;
 
+      //! TODO: remove hard coded UID (for testing)
+      const uid = "mxO6ABVshPM5HGrbmnA1PGpeGAI2"; //user ? user.uid : null;
+
+      if (uid) {
+        const userDocRef = doc(db, "Users", uid);
+        const userDoc = await getDoc(userDocRef);
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+
+          // Get the events for the user
+          for (let i = 0; i < userData.events.length; i++) {
+            const event = userData.events[i];
+            const eventDoc = await getDoc(event);
+            const eventData = eventDoc.data();
+
+            console.log("Event data:", eventData.timestamp);
+          }
+
+        }
+        else { console.log("No such document!"); }
+      } else { console.error("Invalid UID."); }
+    } else { console.error("User is not signed in."); }
+  });
   return (
     <>
       <FullCalendar
         themeSystem='standard'
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, timeGridPlugin]}
         initialView="dayGridMonth"
         customButtons={{
           createEvent: {
@@ -44,16 +61,34 @@ export default function Calendar() {
             },
           }}
           headerToolbar={{
-            left: 'dayGridDay,dayGridWeek,dayGridMonth',
+            left: 'timeGridDay,timeGridWeek,dayGridMonth',
             center: 'title',
             right: 'createEvent today prevYear,prev,next,nextYear'
           }}
-          />
-          <style jsx global>{`
-          .fc .fc-toolbar-title {
-            font-weight: bold;
-          }
-          `}</style>
+
+          //! Event Testing
+          events={[
+            {
+              title: 'Event 1',
+              startTime: '11:00:00',
+              endTime: '11:30:00',
+              startRecur: '2025-03-01',
+              endRecur: '2025-03-02',
+            },
+            {
+              title: 'Event 2',
+              startTime: '12:00:00',
+              endTime: '1:30:00',
+              startRecur: '2025-03-01',
+              endRecur: '2025-03-02',
+            },
+          ]}
+      />
+      <style jsx global>{`
+        .fc .fc-toolbar-title {
+        font-weight: bold;
+        }
+      `}</style>
     </>
   )
 }
