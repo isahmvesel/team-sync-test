7b0a0d22656aa7fb8f99bab0fb51a3d5df590840
dylmill8 <mill3170@purdue.edu>
2025-03-05 22:45:41 -0500

    User auth for calendar events & calendar content scaling for different screen sizes

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -7,7 +7,7 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list';
 import { useRouter } from "next/navigation";
-// import NavBar from "@/components/ui/navigation-bar";
+import NavBar from "@/components/ui/navigation-bar";
 import { firebaseApp } from "@/utils/firebaseConfig";
 import { db } from '@/utils/firebaseConfig';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
@@ -44,12 +44,9 @@ export default function Calendar() {
   const calendarRef = useRef<FullCalendar>(null);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async () => {
-      //! TODO: Fix if statement
-      if (1) { // user) {
-
-        //! TODO: Fix hardcoded UID (For Testing)
-        const uid = 'mxO6ABVshPM5HGrbmnA1PGpeGAI2'; // user.uid;
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const uid = user.uid;
 
         if (uid) {
           const userDocRef = doc(db, "Users", uid);
@@ -92,93 +89,97 @@ export default function Calendar() {
 
   return (
     <>
-      {/* <NavBar /> */}
-      <FullCalendar
-        ref={calendarRef}
-        themeSystem='standard'
-        plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
-        initialView="dayGridMonth"
-        navLinks={true}
-        selectable={true}
+      <NavBar />
+      <div style={{ height: 'calc(80vh)' }}>
+        <FullCalendar
+          ref={calendarRef}
+          themeSystem='standard'
+          plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
+          initialView="dayGridMonth"
+          navLinks={true}
+          selectable={true}
+          height="100%"
+          contentHeight="100%"
 
-        /* Alternate Event Colors
-        eventBackgroundColor="#f2f2f2"
-        eventBorderColor="#3182ce"
-        eventTextColor="black"
-        */
+          /* Alternate Event Colors
+          eventBackgroundColor="#f2f2f2"
+          eventBorderColor="#3182ce"
+          eventTextColor="black"
+          */
 
-        customButtons={{
-          createEvent: {
-            text: 'create event',
-            click: () => {
-              router.push('/event/create');
+          customButtons={{
+            createEvent: {
+              text: 'create event',
+              click: () => {
+                router.push('/event/create');
+              },
             },
-          },
-          list: {
-            text: 'list',
-            click: () => {
-              const calendarApi = calendarRef.current?.getApi();
-              if (calendarApi?.view.type === 'timeGridDay') {
-                calendarApi.changeView('listDay');
-              } else if (calendarApi?.view.type === 'timeGridWeek') {
-                calendarApi.changeView('listWeek');
-              } else if (calendarApi?.view.type === 'dayGridMonth') {
-                calendarApi.changeView('listMonth');
-              } else if (calendarApi?.view.type === 'listDay') {
-                calendarApi.changeView('timeGridDay');
-              } else if (calendarApi?.view.type === 'listWeek') {
-                calendarApi.changeView('timeGridWeek');
-              } else if (calendarApi?.view.type === 'listMonth') {
-                calendarApi.changeView('dayGridMonth');
-              }
-            },
-          }
-        }}
-        headerToolbar={{
-          left: 'list timeGridDay,timeGridWeek,dayGridMonth',
-          center: 'title',
-          right: 'createEvent today prevYear,prev,next,nextYear'
-        }}
-        events={eventList}
-        eventDidMount={(info) => {
-          if (info.event.extendedProps.description && info.view.type !== 'dayGridMonth') {
-            const descEl = document.createElement('p');
+            list: {
+              text: 'list',
+              click: () => {
+                const calendarApi = calendarRef.current?.getApi();
+                if (calendarApi?.view.type === 'timeGridDay') {
+                  calendarApi.changeView('listDay');
+                } else if (calendarApi?.view.type === 'timeGridWeek') {
+                  calendarApi.changeView('listWeek');
+                } else if (calendarApi?.view.type === 'dayGridMonth') {
+                  calendarApi.changeView('listMonth');
+                } else if (calendarApi?.view.type === 'listDay') {
+                  calendarApi.changeView('timeGridDay');
+                } else if (calendarApi?.view.type === 'listWeek') {
+                  calendarApi.changeView('timeGridWeek');
+                } else if (calendarApi?.view.type === 'listMonth') {
+                  calendarApi.changeView('dayGridMonth');
+                }
+              },
+            }
+          }}
+          headerToolbar={{
+            left: 'list timeGridDay,timeGridWeek,dayGridMonth',
+            center: 'title',
+            right: 'createEvent today prevYear,prev,next,nextYear'
+          }}
+          events={eventList}
+          eventDidMount={(info) => {
+            if (info.event.extendedProps.description && info.view.type !== 'dayGridMonth') {
+              const descEl = document.createElement('p');
 
-            descEl.textContent = info.event.extendedProps.description;
-            descEl.style.fontSize = '0.9em';
-            descEl.style.color = 'black';
-            descEl.style.whiteSpace = 'normal';
-            descEl.style.overflowWrap = 'anywhere';
-            descEl.style.margin = '0';
-            info.el.querySelector('.fc-event-title')?.appendChild(descEl);
-          }
-        }}
-        eventMouseEnter={(info) => {
-          if (info.event.extendedProps.description && info.view.type === 'dayGridMonth') {
-            const rect = info.el.getBoundingClientRect();
-            const tooltipEl = document.createElement('div');
-            tooltipEl.classList.add('my-event-tooltip');
-            tooltipEl.innerHTML = info.event.extendedProps.description;
-            tooltipEl.style.position = 'fixed';
-            tooltipEl.style.fontSize = '0.8em';
-            tooltipEl.style.left = `${rect.left}px`;
-            tooltipEl.style.top = `${rect.bottom}px`;
-            tooltipEl.style.zIndex = '9999';
-            tooltipEl.style.backgroundColor = 'white';
-            tooltipEl.style.border = '1px solid #ccc';
-            tooltipEl.style.padding = '5px';
-            tooltipEl.style.whiteSpace = 'normal';
-            document.body.appendChild(tooltipEl);
-            info.event.setExtendedProp('tooltipEl', tooltipEl);
-          }
-        }}
-        eventMouseLeave={(info) => {
-          const tooltipEl = info.event.extendedProps.tooltipEl;
-          if (tooltipEl) {
-            tooltipEl.remove();
-          }
-        }}
-      />
+              descEl.textContent = info.event.extendedProps.description;
+              descEl.style.fontSize = '0.9em';
+              descEl.style.color = 'black';
+              descEl.style.whiteSpace = 'normal';
+              descEl.style.overflowWrap = 'anywhere';
+              descEl.style.margin = '0';
+              info.el.querySelector('.fc-event-title')?.appendChild(descEl);
+            }
+          }}
+          eventMouseEnter={(info) => {
+            if (info.event.extendedProps.description && info.view.type === 'dayGridMonth') {
+              const rect = info.el.getBoundingClientRect();
+              const tooltipEl = document.createElement('div');
+              tooltipEl.classList.add('my-event-tooltip');
+              tooltipEl.innerHTML = info.event.extendedProps.description;
+              tooltipEl.style.position = 'fixed';
+              tooltipEl.style.fontSize = '0.8em';
+              tooltipEl.style.left = `${rect.left}px`;
+              tooltipEl.style.top = `${rect.bottom}px`;
+              tooltipEl.style.zIndex = '9999';
+              tooltipEl.style.backgroundColor = 'white';
+              tooltipEl.style.border = '1px solid #ccc';
+              tooltipEl.style.padding = '5px';
+              tooltipEl.style.whiteSpace = 'normal';
+              document.body.appendChild(tooltipEl);
+              info.event.setExtendedProp('tooltipEl', tooltipEl);
+            }
+          }}
+          eventMouseLeave={(info) => {
+            const tooltipEl = info.event.extendedProps.tooltipEl;
+            if (tooltipEl) {
+              tooltipEl.remove();
+            }
+          }}
+        />
+      </div>
       <style jsx global>{`
         .fc .fc-toolbar-title {
           font-weight: bold;
